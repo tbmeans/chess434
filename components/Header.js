252686de802eddfb5a4bcbed16c7496b8769d82a
Timothy Means <tbmeans2005@gmail.com>
2022-06-18 18:54:12 -0400

    revert header and footer components based on next-netlify-starter

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,3 @@
 export default function Header({ title }) {
-  return (
-    <div id="my-logo" className="navbar-brand">
-      <div className="navbar-item">
-        <img src="icon.png" />
-        <span className="title">{title}</span>
-      </div>
-      <a role="button" id="chessburger" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="chess-menu">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-  )
+  return <h1 className="title">{title}</h1>
 }
