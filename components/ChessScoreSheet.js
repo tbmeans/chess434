94eef94b2e777c076bc58cd782bf9c58006fdce8
Timothy Means <tbmeans2005@gmail.com>
2022-06-18 19:15:50 -0400

    empty out chesssquare and chessmenu functions, remove other components

--- components/ChessScoreSheet.js
+++
@@ -1,8 +0,0 @@
-export default function ChessScoreSheet(props) {
-	return (
-		<div className="box" id="score-sheet">
-			<p id="movetext">{props.movetext}</p>
-			<p id="captures">{props.captures}</p>
-		</div>
-	);
-}
---
+++ components/ListAsMenu.js
@@ -0,0 +1,1 @@
+export default function ListAsMenu(props) {}
