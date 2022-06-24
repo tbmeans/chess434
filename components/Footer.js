dd55833863ad2d0c5c4d08040dffaf544dca9941
Timothy Means <tbmeans2005@gmail.com>
2022-06-24 19:24:23 -0400

    using Wikimedia credit component

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,16 @@
 import styles from './Footer.module.css'
+import WikimediaCredit from './WikimediaCredit'
 
-export default function Footer() {
+export default function Footer({ cred, fnames, params }) {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        {/* Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you */}
+        <p>2022, Tim Means</p>
+        <WikimediaCredit
+          content={cred.c} lang={cred.l} user={cred.u} lic={cred.i} ver={cred.v}
+          mediaFileids={fnames} mediaCurids={params}
+        />
       </footer>
     </>
   )
