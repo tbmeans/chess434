ffb7ddd6960c4728f7c2c61efe0fd3228a54b4f3
Timothy Means <tbmeans2005@gmail.com>
2022-10-09 00:43:47 -0400

    remove commented-out Next.js default title

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,17 @@
 import { header, pageTitle, logo, mobile } from './Header.module.css'
 
 export default function Header({ title }) {
-  {/* return <h1 className="title">{title}</h1> */}
   return (
     <div className={header}>
-      <div><img src="icon.png" className={logo} /></div>
-      <div className={pageTitle}><h1>{title}</h1></div>
-      <div><img src="icon.png" className={[mobile, logo].join(' ')} /></div>
+      <div>
+        <img src="icon.png" className={logo} />
+      </div>
+      <div className={pageTitle}>
+        <h1>{title}</h1>
+      </div>
+      <div>
+        <img src="icon.png" className={[ mobile, logo ].join(' ')} />
+      </div>
     </div>
   );
 }
