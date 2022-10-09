1524c594c02961cda8e1d92c32a159d87af4480d
Timothy Means <tbmeans2005@gmail.com>
2022-10-09 00:56:23 -0400

    refactored footer data as JSON

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,23 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import GameGrid from '@components/GameGrid'
 
-const cred = {
-  c: "Chess vector images",
-  l: "en",
-  u: "User:Cburnett",
-  i: 'by-sa',
-  v: '3.0'
-};
+const cred = JSON.stringify({
+  content: "Chess vector images",
+  lang: "en",
+  user: "User:Cburnett",
+  lic: 'by-sa',
+  ver: '3.0'
+});
 
-const fnames = [ 'kdt', 'klt', 'qdt', 'qlt', 'rdt', 'rlt',
-'bdt', 'blt', 'ndt', 'nlt', 'pdt', 'plt' ];
+const fnames = JSON.stringify([
+  'kdt', 'klt', 'qdt', 'qlt', 'rdt', 'rlt',
+  'bdt', 'blt', 'ndt', 'nlt', 'pdt', 'plt'
+]);
 
-const params = [ 1499803, 1499806, 1499811, 1499812, 1499813, 1499814,
-1499800, 1499801, 1499807, 1499808, 1499809, 1499810 ];
+const params = JSON.stringify([
+  1499803, 1499806, 1499811, 1499812, 1499813, 1499814,
+  1499800, 1499801, 1499807, 1499808, 1499809, 1499810
+]);
 
 export default function Home() {
   return (
