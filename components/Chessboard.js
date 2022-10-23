6dd3c6a047eeae5100141cfacd7b152eb7a6f70a
Timothy Means <tbmeans2005@gmail.com>
2022-10-23 04:37:18 -0400

    fixed pawn promotion; refactored logic, attribs, names

--- components/Chessboard.js
+++ components/Chessboard.js
@@ -11,10 +11,6 @@ export default function Chessboard(props) {
     ).map( pcn => pcn.slice(2, 4) )
   );
 
-  const selSq = props.org ? (
-    "abcdefgh".indexOf(props.org[0]) - 8 * props.org[1] + 64
-  ) : '';
-
   const piece = Object.freeze({
     b: 'bishop', k: 'king', n: 'knight', p: 'pawn', q: 'queen', r: 'rook'
   });
@@ -29,99 +25,97 @@ export default function Chessboard(props) {
         const shade = (x + y) % 2 === 1 ? "Lite" : "Dark";
         const file = "abcdefgh"[x];
         const rank = "12345678"[y];
-        const hasMoves = origins.includes(file + rank);
-        const isSelectedSquare = file + rank === props.org;
+        const sq = file + rank;
+        const hasMoves = origins.includes(sq);
+        const isSelectedSquare = sq === props.org;
+        const isTargetSquare = movesFromSel.includes(sq);
 
-        const bg = (
-          [ styles[props.theme + shade] ].concat(
-            isSelectedSquare ? (
-              (hasMoves ? [ styles.moveAvail ] : [ styles.noMove ])
-            ) : []
-          )
+        const bg = [ styles[props.theme + shade] ].concat(
+          isSelectedSquare ? (
+            (hasMoves ? [ styles.moveAvail ] : [ styles.noMove ])
+          ) : []
         ).join(' ');
 
-        const fg = file === 'a' || rank == 1 ? (
+        const isPromotionMove = (
+          activeColorIsWhite && rank == 8 ||
+          activeColorIsWhite === false && rank == 1
+        ) && props.pieceOn(props.org, ppd64) === (
+          activeColorIsWhite ? 'P' : 'p'
+        );
+
+        const select = (props.isActive || null) && ( () => {
+          if (isTargetSquare) {
+            if (isPromotionMove) {
+              props.setTgt(sq);
+              props.setGame(props.isHeld);
+              props.openMenu(true);
+              props.setMenu('1');
+            } else {
+              const pcn = props.org + sq;
+              props.setSeq( s => props.seqIncr(s, pcn) );
+              props.setOrg('');
+            }
+          } else if (isSelectedSquare) {
+            props.setOrg('');
+          } else {
+            props.setOrg(sq);
+          }
+          return;
+        });
+
+        const fg = (file === 'a' || rank == 1) && (
           [ styles.anLabel ].concat(
             ( file === 'a' && rank % 2 == 1 ||
-              rank == 1 && 'aceg'.includes(file) ) ?
-            [ styles.onDark ] : []
+              rank == 1 && 'aceg'.includes(file)
+            ) && [ styles.onDark ] || []
           ).join(' ')
-        ) : '';
+        ) || '';
 
         const text = '\u00A0' + (
           (rank == 1 ? file : '') +
           (file === 'a' ? rank : '')
         );
 
-        const isTargetSquare = movesFromSel.includes(file + rank);
+        const boardLabel = fg.length && (
+          <span className={fg}>{text}</span>
+        ) || null;
 
-        const isPromotionMove = (
-          isTargetSquare && (
-            activeColorIsWhite && rank == 8 ||
-            activeColorIsWhite === false && rank == 1
-          ) &&
-          props.pieceOn(selSq, ppd64) === (activeColorIsWhite ? 'P' : 'p')
-        );
+        const path = (isTargetSquare || n != 1) && (
+          n == 1 && "dot.svg" || (
+            "Chess_" +
+            n.toLowerCase() +
+            (n.toUpperCase() === n ? "lt45" : "dt45") +
+            (isTargetSquare && "_dotted" || '') +
+            ".svg"
+          )
+        ) || '';
 
-        const imgTitle = (n == 1 ?
-          [] : [
+        const imgLabel = (
+          n == 1 && [] || [
             n.toUpperCase() === n ? "white" : "black",
             piece[n.toLowerCase()],
             "on"
           ]
-        ).concat([ file + rank ]).join(' ') +
+        ).concat([ sq ]).join(' ') +
         (isTargetSquare ? ", legal move" : '') +
         (isSelectedSquare ? " selected" : '');
 
-        const path = n == 1 ? (
-          isTargetSquare ? "dot.svg" : ''
-        ) : (
-          "Chess_" +
-          n.toLowerCase() +
-          (n.toUpperCase() === n ? "lt45" : "dt45") +
-          (isTargetSquare ? "_dotted" : '') +
-          ".svg"
-        );
-
-        const label = fg.length ? <span className={fg}>{text}</span> : null;
-
-        const image = path.length ? (
-          <img
-            src={path}
-            title={imgTitle}
-            className={styles.piece}
-          />
-        ) : null;
+        const image = path.length && (
+          <img src={path} alt={imgLabel} className={styles.piece} />
+        ) || null;
 
         return (
-          <div key={file + rank}
-            id={file + rank}
-            title={file + rank}
-            className={bg}
-            onClick={ (props.isActive || null) && ( () => {
-              if (isTargetSquare) {
-                if (isPromotionMove) {
-                  props.setTgt(file + rank);
-                  props.setGame(props.isHeld);
-                } else {
-                  const pcn = props.org + file + rank;
-                  props.setSeq( s => props.seqIncr(s, pcn) );
-                  props.setOrg('');
-                }
-              } else if (isSelectedSquare) {
-                props.setOrg('');
-              } else {
-                props.setOrg(file + rank);
-              }
-              return;
-            })}
-          >{label}{image}</div>
+          <div key={sq} className={bg} onClick={select}>
+            {boardLabel}{image}
+          </div>
         );
       }
     )
   );
 
   return (
-    <div className={styles.grid8Cols}>{squares}</div>
+    <div className={styles.grid8Cols}>
+      {squares}
+    </div>
   );
 }
