0b36a727b20bb0239812d7f632747f6a93c42594
Timothy Means <tbmeans2005@gmail.com>
2022-10-23 04:45:26 -0400

    added alt attrib to imgs

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
 import { header, pageTitle, logo, mobile } from './Header.module.css'
 
 export default function Header({ title }) {
+  const mobileLogo = [ mobile, logo ].join(' ');
   return (
     <div className={header}>
       <div>
-        <img src="icon.png" className={logo} />
+        <img src="icon.png" alt="IC 434" className={logo} />
       </div>
       <div className={pageTitle}>
-        <h1>{title}</h1>
+        <h1>
+          {title}
+        </h1>
       </div>
       <div>
-        <img src="icon.png" className={[ mobile, logo ].join(' ')} />
+        <img src="icon.png" alt="IC 434" className={mobileLogo} />
       </div>
     </div>
   );
